b6e431b4c4266c344b56c59d91f6dc0c011af5b1
Francesca Marciani <fmarciani@gmail.com>
2018-04-11 13:33:38 -0500

    Added User class.

---
+++ OnboardingExperience/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnboardingExperience
+{
+    class User
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int PIN { get; set; }
+
+        /*static string AskQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }*/
+
+        //static int AskIntQuestion(int age)
+    }
+}
