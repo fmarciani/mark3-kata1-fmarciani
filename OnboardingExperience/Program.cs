09157e50c1afe1a56092efbd995ffb0e4a1b1f88
Francesca Marciani <fmarciani@gmail.com>
2018-04-13 08:31:15 -0500

    Edited error catch for AskQuestion method. Edited console prompts.

--- OnboardingExperience/Program.cs
+++ OnboardingExperience/Program.cs
@@ -8,29 +8,30 @@ namespace OnboardingExperience
         {
             var user = new User();
 
-            user.IsAccountOwner = AskBoolQuestion("\nHello! Before we can create your account, we need some information from you. Are you the account owner? Please enter y for yes or n for no.");
+            user.IsAccountOwner = AskBoolQuestion("\nWelcome, and thank you for choosing IntelliBank! Before we can create your account, we need some information from you.\nAre you the account owner? Please enter y for yes or n for no.");
 
             if (!user.IsAccountOwner)
             {
                 Environment.Exit(-1);
             }
 
-            user.FirstName = AskQuestion("\nWelcome, and thank you for choosing IntelliBank! Let's get your account set up. What is your first name?");
+            user.FirstName = AskQuestion("\nGreat! Let's get your account set up. What's your first name?");
             user.LastName = AskQuestion($"\nHi there, {user.FirstName}! Now we need to grab your last name. Go ahead and type it below.");
 
-            Console.WriteLine($"\nGotcha! Your name is {user.FullName}. Let's keep going. Press any key to continue:");
+            Console.WriteLine($"\nSuccess! Your name is {user.FullName}. Let's keep going. Press any key to continue:");
             Console.ReadKey(true);
 
-            user.Age = AskIntQuestion("\nNext, we'll need to know your age. Please enter it as a number (e.g., 25).", 2);
-            Console.WriteLine($"\nCool! You're {user.Age} years old. Press any key to move on.");
+            // Code discriminates against centenarians and elementary schoolers.
+            user.Age = AskIntQuestion($"\nUh, oh; it's time to get personal, {user.FullName}: We need to know your age. Please enter it as a number (e.g., 25).", 2);
+            Console.WriteLine($"\nYou're {user.Age} years young, and a captive audience to our dad jokes. Press any key to move on.");
             Console.ReadKey(true);
 
-            user.PIN = AskIntQuestion($"\nNow we're going to have you enter a 4-digit PIN to help protect your account. This can be any number that's easy for you to remember, but please don't choose a PIN that isn't easy to guess (e.g., 2222, 1234). Go ahead and enter your chosen PIN now, {user.FirstName}!", 4);
-            Console.WriteLine($"\nYou chose **** as your PIN. (Don't worry; we won't tell.) You'll need to reenter it every time you log into your account, so don't lose it! Press any key to keep going.");
+            user.PIN = AskIntQuestion($"\nNow we're going to have you enter a 4-digit PIN to help protect your account. Try not to choose a PIN that's too easy\nto guess (e.g., 2222, 1234), and don't lose it! You'll need to reenter it every time you log into your account. Go\nahead and enter your chosen PIN now, {user.FirstName}!", 4);
+            Console.WriteLine($"\nYou chose **** as your PIN. (Don't worry; we won't tell.) Press any key to keep going.");
             Console.ReadKey(true);
 
-            user.SecurePhrase = AskQuestion($"\nHuff, puff; we're almost done here! Final round: Let's add a security phrase to your account for extra security! It can be anything (again, we won't tell), but you'll have to type it EXACTLY the way you do here each time you log in. You've got this, {user.FirstName}. Go ahead and enter your phrase below.");
-            Console.WriteLine($"\nHey, hey! Look who's good to go. We've got your name, age, PIN, and security phrase ({user.SecurePhrase}), in case you forgot). We won't get more personal than that. Thanks again for banking with IntelliBank! Press any key to exit the app.");
+            user.SecurePhrase = AskQuestion($"\nHuff, puff; we're almost done here! Final round: Let's add a security phrase to your account. It can be anything, but\nyou'll have to type it EXACTLY the way you do here each time you log in. You've got this, {user.FirstName}. Go ahead and enter your\nphrase below.");
+            Console.WriteLine($"\nHey, hey! Look who's good to go. Don't worry, we're done getting personal. Thanks again for choosing IntelliBank! Press\nany key to exit the app.");
             Console.ReadKey(true);
             Environment.Exit(-1);
         }
@@ -44,8 +45,6 @@ namespace OnboardingExperience
 
                 if (response == "y" || response == "yes")
                 {
-                    Console.WriteLine("\nGreat! Press any key to continue.");
-                    Console.ReadKey(true);
                     return true;
                 }
                 else if (response == "n" || response == "no")
@@ -68,7 +67,7 @@ namespace OnboardingExperience
 
             if (answer.Length == 0)
             {
-                return "Julian McBoolean";
+                return "Doot";
             }
 
             else
